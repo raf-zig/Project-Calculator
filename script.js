f6dde9ac3495e9c438195122729e050399227375
raf-zig <ziravin@yandex.ru>
2022-12-21 20:32:59 +0500

    add math functions

---
+++ script.js
@@ -0,0 +1,15 @@
+function add(first_digit, second_digit){
+    return first_digit + second_digit;
+};
+function subtract(first_digit, second_digit) {
+    return first_digit - second_digit;
+};
+function multiply(first_digit, second_digit) {
+    return first_digit * second_digit;
+};
+function divide(first_digit, second_digit) {
+    return first_digit / second_digit;
+};
+function  operate(operator, first_digit, second_digit) {
+
+};
