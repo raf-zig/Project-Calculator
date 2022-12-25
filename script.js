4ec31073cc16034316846340bd7fc4dee09af483
raf-zig <ziravin@yandex.ru>
2022-12-25 15:07:14 +0500

    add output function

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let first_digit = '';
+let second_digit = '';
+
+
+
 function add(first_digit, second_digit){
     return first_digit + second_digit;
 };
@@ -11,6 +16,18 @@ function divide(first_digit, second_digit) {
     return first_digit / second_digit;
 };
 
+function insert (num) {
+    document.form.text_view.value = document.form.text_view.value + num;
+}
 
+let items = document.querySelectorAll('.item');
+items.forEach((item) => {
+    item.addEventListener('click', function(e) {
+        let el = e.target.textContent;
+        first_digit += el
+        console.log(first_digit)
+        insert(el)
+    })
+})
 
 
