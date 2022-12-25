99ab533ee1978ab6c02fa7470615bee24f33bdcc
raf-zig <ziravin@yandex.ru>
2022-12-25 19:51:14 +0500

    2.add operate function

--- script.js
+++ script.js
@@ -22,32 +22,40 @@ function insert(num) {
 
 function clear(){
     document.form.text_view.value = '';
-}
+};
 
 let operator = document.querySelectorAll('.sign');
 operator.forEach((signs) => {
     signs.addEventListener('click', function(e) {
-         clear();
+        clear();
         let el = e.target.textContent;
         sign = el;
-        insert(el)
+        insert(el);
+        first_digit = second_digit;
+        second_digit = '';
+        console.log(first_digit)
+        
     })
-   
-
 })
 
 let items = document.querySelectorAll('.number');
     items.forEach((item) => {
         item.addEventListener('click', function(e) {
-            let el = Number(e.target.textContent);
-
-            first_digit += el
+            let el = e.target.textContent;
+            second_digit += el;
             insert(el);
+            console.log(second_digit)
         })
     })
 
 function operate (first_digit, second_digit, sign) {
     if (sign === '+'){
-        insert(add(first_digit, second_digit))
+        insert(add(first_digit, second_digit));
+    } else if (sign === '-') {
+        insert(subtract(first_digit, second_digit));
+    } else if (sign === '*') {
+        insert(multiply(first_digit, second_digit));
+    } else if (sign === '/') {
+        insert(divide(first_digit, second_digit)); 
     }
 }
